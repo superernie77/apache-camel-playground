a73cbd49e65d8775def29b28791ae38db743b6f8
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-11-03 13:22:44 +0100

    fixed more tests

---
+++ src/main/java/com/wds/example/camel/cameldemo/filter/ServiceBean.java
@@ -0,0 +1,15 @@
+package com.wds.example.camel.cameldemo.filter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceBean {
+    public User response(User user) {
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+    
+   public String hello(String name) {
+           return "Hello "+name;
+   }
+}
---
+++ src/main/java/com/wds/example/camel/cameldemo/filter/User.java
@@ -0,0 +1,16 @@
+package com.wds.example.camel.cameldemo.filter;
+
+public class User {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
