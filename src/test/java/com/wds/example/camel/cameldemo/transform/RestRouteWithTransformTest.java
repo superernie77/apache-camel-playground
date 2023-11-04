ce4a7fc0d47bc105c3953b083146bba710226838
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-11-04 12:31:18 +0100

    Test with ProducerTemplate

--- src/test/java/com/wds/example/camel/cameldemo/transform/RestRouteWithTransformTest.java
+++ src/test/java/com/wds/example/camel/cameldemo/transform/RestRouteWithTransformTest.java
@@ -2,7 +2,12 @@ package com.wds.example.camel.cameldemo.transform;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.FluentProducerTemplate;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.impl.engine.DefaultFluentProducerTemplate;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +20,10 @@ class RestRouteWithTransformTest {
 
 	private RestTemplate template = new RestTemplate();
 	
+	@Autowired
+	private CamelContext context;
+	
+	
 	@Test
 	void testHelloWithProcessor() {
 		
@@ -25,5 +34,16 @@ class RestRouteWithTransformTest {
 			String result = template.getForObject(uri.toUriString(), String.class);
 			
 			assertTrue(result.equals("Body tranformed"));	
+	}
+	
+	@Test
+	public void testWithDirectRouteCall() {
+		ProducerTemplate prodTemplate = context.createProducerTemplate();
+		
+		// call the transform route directly without rest call
+		Object result = prodTemplate.requestBody("direct:transform", "");
+		
+		assertTrue(result.equals("Body tranformed"));
+		
 	}
 }
